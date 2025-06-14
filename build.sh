4be3c5b9d486f0cccc8d65b3bb3b8d31bd9b1538
Guilhem Bonnefous <guilhem.bonnefous@etu.u-bordeaux.fr>
2025-06-14 20:20:13 +0200

    Created redirect.html

--- build.sh
+++ build.sh
@@ -10,3 +10,7 @@ npx tailwindcss -i ./src/style.css -o ./dist/output.css --minify
 cp src/manifest.json dist/
 cp src/*.html dist/
 cp src/*.js dist/
+
+# Copy assets
+mkdir -p dist/assets/fonts
+cp -r src/assets/fonts/* dist/assets/fonts/
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        signika: ["Signika", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
