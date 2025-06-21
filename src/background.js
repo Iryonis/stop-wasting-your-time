ea44383d3b5d0a524f91e5cb38b9de0697a51fdf
Guilhem Bonnefous <guilhem.bonnefous@etu.u-bordeaux.fr>
2025-06-21 22:54:31 +0200

    Created countdown (discussion between popup and background.js) + small change on popup and redirect.html

--- src/background.js
+++ src/background.js
@@ -1,12 +1,307 @@
+let countdownInterval = null; // Interval for the countdown
+let countdownDefaultTime = 15 * 60; // Default value for the countdown in seconds
+
+/**
+ * Calls the function 'handleTabChange' when the active tab changes.
+ * This function checks the URL of the active tab and starts or pauses the countdown accordingly.
+ */
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  const tab = await chrome.tabs.get(activeInfo.tabId);
+  handleTabChange(tab.url);
+});
+
+/**
+ * 1. Calls the function 'handleTabChange' when the tab is changed or updated.
+ * It waits for the tab to be fully loaded before checking the URL.
+ *
+ * 2. If the URL contains "youtube.com" and the parameter 'redirectEnabled' (from chrome storage) is at 'true',
+ * it injects the redirect script.
+ */
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url.includes("youtube.com")) {
-    chrome.storage.sync.get(["redirectEnabled"], (data) => {
-      if (data.redirectEnabled) {
-        chrome.scripting.executeScript({
-          target: { tabId: tabId },
-          files: ["redirect.js"],
-        });
+  // 1.
+  if (changeInfo.status === "complete") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs[0] && tabs[0].id === tabId) {
+        handleTabChange(tab.url);
       }
     });
+
+    // 2.
+    if (tab.url && tab.url.includes("youtube.com")) {
+      chrome.storage.sync.get(["redirectEnabled"], (data) => {
+        if (data.redirectEnabled) {
+          chrome.scripting
+            .executeScript({
+              target: { tabId: tabId },
+              files: ["redirect.js"],
+            })
+            .catch((err) => console.log("Injection error (onUpdated):", err));
+        }
+      });
+    }
   }
 });
+
+/**
+ * Handles the change of the active tab.
+ * If the URL contains "youtube.com/shorts/", it starts or resumes the countdown.
+ * If the URL does not contain "youtube.com/shorts/", it pauses the countdown.
+ * @param {string} url - The URL of the active tab
+ */
+const handleTabChange = async (url) => {
+  if (url && url.includes("youtube.com/shorts/")) {
+    console.log("YouTube Shorts - start the countdown");
+    await startOrResumeCountdown();
+  } else {
+    console.log("Not on Shorts - pause the countdown");
+    await pauseCountdown();
+  }
+};
+
+/**
+ * Handles the start, resume, or reset of the countdown based on the current state.
+ *
+ * 1. It checks if the countdown is active and if the last reset date matches today.
+ *    If the last reset date does not match today, it resets the countdown and starts a new one.
+ *
+ * 2. If the countdown is active and has remaining time, it resumes the countdown.
+ *
+ * 3. If the countdown is not active, it starts a new countdown with the duration set in storage.
+ * @returns {Promise<void>} - Starts (/and reset) or resumes the countdown based on the current state.
+ */
+const startOrResumeCountdown = async () => {
+  const storage = await chrome.storage.local.get([
+    "countdownActive",
+    "countdownRemaining",
+    "lastResetDate",
+  ]);
+  const today = new Date().toDateString();
+
+  // 1.
+  if (storage.lastResetDate !== today) {
+    await resetDailyCountdown();
+    await startNewCountdown();
+    console.log("Reset + start");
+    return;
+  }
+
+  // 2.
+  if (storage.countdownActive && storage.countdownRemaining > 0) {
+    await resumeCountdown();
+    console.log("Resume");
+  } else {
+    // 3.
+    await startNewCountdown();
+    console.log("Start");
+  }
+};
+
+/**
+ * Starts a new countdown with the duration set in storage.
+ * If no duration is set, it defaults to 'countdownDefaultTime'.
+ * It initializes the countdown state in local storage and starts the countdown ticker.
+ *
+ * All duration values are in seconds.
+ */
+const startNewCountdown = async () => {
+  const data = await chrome.storage.sync.get(["countdownDurationNext"]);
+  const duration = data.countdownDurationNext || countdownDefaultTime;
+
+  await chrome.storage.local.set({
+    countdownActive: true,
+    countdownDuration: duration,
+    countdownRemaining: duration,
+    isPaused: false,
+    lastResetDate: new Date().toDateString(),
+  });
+
+  startCountdownTicker();
+  console.log(`New countdown: ${duration / 60} minutes`);
+};
+
+/**
+ * Starts the countdown ticker that updates every second.
+ * It checks if the countdown is paused before decrementing the remaining time.
+ * If the remaining time reaches zero, it finishes the countdown.
+ * If the countdown is still active, it decrements the remaining time by one second.
+ */
+const startCountdownTicker = () => {
+  if (countdownInterval) clearInterval(countdownInterval);
+
+  countdownInterval = setInterval(async () => {
+    const storage = await chrome.storage.local.get([
+      "countdownRemaining",
+      "isPaused",
+    ]);
+
+    // If paused, do not decrement
+    if (storage.isPaused) return;
+
+    const remaining = storage.countdownRemaining - 1;
+
+    if (remaining <= 0) {
+      await finishCountdown();
+    } else {
+      await chrome.storage.local.set({ countdownRemaining: remaining });
+    }
+  }, 1000);
+};
+
+/**
+ * Pauses the countdown.
+ * It sets the 'isPaused' flag in local storage to true.
+ */
+const pauseCountdown = async () => {
+  await chrome.storage.local.set({ isPaused: true });
+  console.log("Countdown paused.");
+};
+
+/**
+ * Resumes the countdown if it was paused.
+ * It sets the 'isPaused' flag in local storage to false and starts the countdown ticker if it wasn't already running.
+ */
+const resumeCountdown = async () => {
+  await chrome.storage.local.set({ isPaused: false });
+  if (!countdownInterval) {
+    startCountdownTicker();
+  }
+  console.log("Countdown resumed.");
+};
+
+/**
+ * Finishes the countdown.
+ *
+ * It clears the countdown interval, sets the countdown state in local storage to inactive,
+ * and enables redirection for YouTube tabs.
+ * It also activates the redirection script on existing YouTube tabs.
+ */
+const finishCountdown = async () => {
+  if (countdownInterval) {
+    clearInterval(countdownInterval);
+    countdownInterval = null;
+  }
+
+  await chrome.storage.local.set({
+    countdownActive: false,
+    countdownRemaining: 0,
+    isPaused: false,
+  });
+
+  await chrome.storage.sync.set({ redirectEnabled: true });
+  console.log("Countdown finished - redirection enabled");
+
+  activateRedirectionOnExistingTabs();
+};
+
+/**
+ * Resets the daily countdown.
+ * It clears the countdown interval, sets the countdown state in local storage to inactive,
+ * and resets the last reset date to today.
+ */
+const resetDailyCountdown = async () => {
+  if (countdownInterval) {
+    clearInterval(countdownInterval);
+    countdownInterval = null;
+  }
+
+  await chrome.storage.local.set({
+    countdownActive: false,
+    countdownRemaining: 0,
+    isPaused: false,
+    lastResetDate: new Date().toDateString(),
+  });
+
+  await chrome.storage.sync.set({ redirectEnabled: false });
+  console.log("Countdown reset - redirection disabled");
+};
+
+/**
+ * Initializes the countdown.
+ *
+ * If the last reset date does not match today, it resets the countdown.
+ * If the countdown is active and has remaining time, it starts the countdown ticker and pause it.
+ * @returns {Promise<void>} - Initializes the countdown by checking the last reset date and starting
+ * the countdown if necessary.
+ */
+const initializeCountdown = async () => {
+  const storage = await chrome.storage.local.get([
+    "countdownActive",
+    "countdownRemaining",
+    "lastResetDate",
+  ]);
+
+  const today = new Date().toDateString();
+  if (storage.lastResetDate !== today) {
+    await resetDailyCountdown();
+    return;
+  }
+
+  if (storage.countdownActive && storage.countdownRemaining > 0) {
+    await chrome.storage.local.set({ isPaused: true });
+    startCountdownTicker();
+    console.log("Countdown initialized and paused.");
+  }
+};
+
+/**
+ * Listens for messages from the popup or other parts of the extension.
+ *
+ * 'getCountdownStatus' action retrieves the current countdown status from local storage
+ * and sends it back to the sender.
+ */
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  switch (message.action) {
+    case "getCountdownStatus":
+      chrome.storage.local.get(
+        [
+          "countdownActive",
+          "countdownRemaining",
+          "countdownDuration",
+          "isPaused",
+        ],
+        (data) => {
+          sendResponse(data);
+        }
+      );
+      return true;
+  }
+});
+
+/**
+ * Initializes the countdown and checks the current tab.
+ */
+initializeCountdown().then(() => {
+  checkCurrentTab();
+});
+
+/**
+ * Checks the current active tab and handles the URL change.
+ */
+const checkCurrentTab = async () => {
+  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+  if (tabs[0]) {
+    handleTabChange(tabs[0].url);
+  }
+};
+
+/**
+ * Activates the redirection script on all existing YouTube tabs.
+ * It queries all tabs with a URL matching "www.youtube.com" and injects the redirect script into each of them.
+ */
+const activateRedirectionOnExistingTabs = () => {
+  chrome.tabs.query({ url: "*://www.youtube.com/*" }, (tabs) => {
+    tabs.forEach((tab) => {
+      chrome.scripting
+        .executeScript({
+          target: { tabId: tab.id },
+          files: ["redirect.js"],
+        })
+        .catch((err) =>
+          console.log(
+            "Injection error (activateRedirectionOnExistingTabs):",
+            err
+          )
+        );
+    });
+  });
+};
--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,4 @@
+// Dictionary for slider values
 let sliderValues = {
   0: 0.2,
   25: 15,
@@ -6,15 +7,28 @@ let sliderValues = {
   100: 120,
 };
 
-let chronoTime = 30; // Valeur par défaut pour le chrono (en minutes)
-let countdownInterval = null; // pour clearInterval plus tard
-let timeDown = false; // pour savoir si le temps est écoulé
+let countdownDefaultTime = 15; // Default value for the countdown in minutes
+let countdownTime; // Current countdown time in minutes
+let displayUpdateInterval = null; // Display update interval
+let isActive = false; // to track if the countdown is active
 
-formatTextFromSlider = (sliderValue) => {
+/**
+ * Format the slider value to the corresponding time in minutes, using a dictionary.
+ * If the value is not found, it defaults to 0.
+ * @param {number} sliderValue - The value from the slider (0-100)
+ * @returns {number} - The corresponding time in minutes
+ */
+const formatTextFromSlider = (sliderValue) => {
   return sliderValues[sliderValue] || 0;
 };
 
-formatTimeInString = (time) => {
+/**
+ * Format the time in minutes to a string representation (e.g., "1h 30m").
+ * If the time is 0, it returns "0m".
+ * @param {number} time - The time in minutes
+ * @returns {string} - The formatted time string
+ */
+const formatTimeInString = (time) => {
   let hours = Math.floor(time / 60);
   let minutes = time % 60;
   let result = [];
@@ -24,89 +38,147 @@ formatTimeInString = (time) => {
   return result.join(" ");
 };
 
-// ➕ Affiche hh:mm:ss à partir de secondes
-formatTimeFull = (totalSeconds) => {
-  const h = Math.floor(totalSeconds / 3600);
-  const m = Math.floor((totalSeconds % 3600) / 60);
-  const s = totalSeconds % 60;
+/**
+ * Format the time in seconds into a full time string (HH:MM:SS) (used by the countdown).
+ * @param {number} time - The time in seconds
+ * @returns {string} - The formatted time string in HH:MM:SS format
+ */
+const formatTimeFull = (time) => {
+  const h = Math.floor(time / 3600);
+  const m = Math.floor((time % 3600) / 60);
+  const s = time % 60;
   return [h, m, s].map((v) => v.toString().padStart(2, "0")).join(":");
 };
 
-// ➕ Lancer le chrono avec chronoTime (en minutes)
-startCountdown = () => {
-  if (countdownInterval) clearInterval(countdownInterval);
-  console.log(timeDown);
+/**
+ * Starts the display update interval to refresh the countdown display every second.
+ * It retrieves the countdown status from the background script and updates the display accordingly.
+ * If the countdown is active, it shows the remaining time in green; if paused, it shows the time in orange;
+ * if inactive, it shows the default time in gray and stops the interval.
+ */
+const startDisplayUpdate = () => {
+  if (displayUpdateInterval) clearInterval(displayUpdateInterval);
 
-  const display = document.getElementById("countdown-display");
-  const durationMs = chronoTime * 60 * 1000;
-  const endTime = Date.now() + durationMs;
+  displayUpdateInterval = setInterval(() => {
+    chrome.runtime.sendMessage({ action: "getCountdownStatus" }, (status) => {
+      if (!status) return;
 
-  function tick() {
-    const now = Date.now();
-    let remaining = Math.round((endTime - now) / 1000);
-    if (remaining < 0) remaining = 0;
+      const display = document.getElementById("countdown-display");
 
-    display.textContent = formatTimeFull(remaining);
-
-    if (remaining === 0) {
-      clearInterval(countdownInterval);
-      timeDown = true;
-      // Active la redirection dans le storage
-      chrome.storage.sync.set({ redirectEnabled: true }, () => {
-        console.log("Redirection activée");
-        // Injecte immédiatement le script sur tous les onglets YouTube ouverts
-        activateRedirectionOnExistingTabs();
-      });
-    }
-  }
-
-  tick(); // affichage immédiat
-  countdownInterval = setInterval(tick, 1000);
+      if (status.countdownActive) {
+        const timeText = formatTimeFull(status.countdownRemaining || 0);
+        if (status.isPaused) {
+          // If paused, display in orange
+          display.textContent = timeText;
+          display.style.color = "#f59e0b";
+        } else {
+          // If active, display in green
+          display.textContent = timeText;
+          display.style.color = "#10b981";
+        }
+      } else {
+        // If inactive, display the default time in gray
+        display.textContent = formatTimeFull(countdownTime * 60);
+        display.style.color = "#6b7280";
+        stopDisplayUpdate();
+      }
+    });
+  }, 1000);
 };
 
-// Fonction pour activer la redirection sur les onglets YouTube déjà ouverts
-function activateRedirectionOnExistingTabs() {
-  chrome.tabs.query({ url: "*://www.youtube.com/*" }, (tabs) => {
-    tabs.forEach((tab) => {
-      chrome.scripting
-        .executeScript({
-          target: { tabId: tab.id },
-          files: ["redirect.js"],
-        })
-        .catch((err) => console.log("Erreur injection:", err));
-    });
-  });
-}
+/**
+ * Stops the display update interval if it is currently running.
+ * This is used to clean up the interval when the countdown is not active.
+ */
+const stopDisplayUpdate = () => {
+  if (displayUpdateInterval) {
+    clearInterval(displayUpdateInterval);
+    displayUpdateInterval = null;
+  }
+};
+
+/**
+ * Get the slider value and updates the slider text based on the current value of the slider.
+ * @param {element} slider - The slider input element
+ * @param {element} valueText - The text element to display the slider value
+ */
+const updateSliderText = (slider, valueText) => {
+  const time = formatTextFromSlider(Number(slider.value));
+  valueText.textContent = formatTimeInString(time);
+  countdownTime = time;
+};
 
 document.addEventListener("DOMContentLoaded", () => {
-  const slider = document.getElementById("slider-input");
-  const valueText = document.getElementById("slider-value-text");
-  const updateButton = document.getElementById("update-button");
+  // Get elements from the DOM
+  const slider = document.getElementById("slider-input"); // Slider input element
+  const valueText = document.getElementById("slider-value-text"); // Text to display the slider value
+  const display = document.getElementById("countdown-display"); // Countdown display element
+  const updateButton = document.getElementById("update-button"); // Button to update the countdown
 
-  valueText.innerText = slider.value;
+  // Initialize elements with default values
+  display.textContent = formatTimeFull(countdownDefaultTime * 60);
+  updateSliderText(slider, valueText);
 
-  function updateSliderText() {
-    valueText.textContent = formatTimeInString(
-      formatTextFromSlider(Number(slider.value))
-    );
-  }
-
-  slider.addEventListener("input", updateSliderText);
-  updateSliderText(); // Initialisation au chargement
-
-  updateButton.addEventListener("click", () => {
-    // Correction de l'appel à updateChrono
-    updateChrono();
+  /**
+   * Retrieve the countdown duration from storage and update the display and slider.
+   *
+   * If a countdown duration is found, it updates the display and slider to reflect the stored value.
+   * If no countdown duration is found, it uses the default countdown time.
+   * This is done to ensure that the countdown starts with the correct value when the popup is opened.
+   */
+  chrome.storage.sync.get(["countdownDuration"], (data) => {
+    if (data.countdownDuration) {
+      countdownTime = data.countdownDuration;
+      display.textContent = formatTimeFull(countdownTime * 60);
+      const sliderVal =
+        Object.keys(sliderValues).find(
+          (key) => sliderValues[key] === countdownTime
+        ) || 50;
+      slider.value = sliderVal;
+      updateSliderText(slider, valueText);
+    }
   });
 
-  // Correction de updateChrono pour prendre la valeur du slider
-  window.updateChrono = () => {
-    chronoTime = formatTextFromSlider(Number(slider.value));
-    timeDown = false; // Réinitialiser le flag
-    startCountdown();
-  };
+  slider.addEventListener("input", (event) => {
+    updateSliderText(slider, valueText);
+  });
 
-  chrome.storage.sync.get(["redirectEnabled"], (data) => {
-    timeDown = data.redirectEnabled || false;
+  /**
+   * Event listener for the update button.
+   *
+   * When clicked, it retrieves the value from the slider
+   * and formats it to seconds.
+   * If the countdown is not active, it formats the time
+   * to a full time string (HH:MM:SS) and updates the display.
+   * Then it saves the new countdown time to storage.
+   */
+  updateButton.addEventListener("click", () => {
+    // Retrieve the value from the slider and format it to seconds
+    countdownTime = formatTextFromSlider(Number(slider.value)) * 60; // In seconds
+
+    // If the countdown is not active, update the display with the formatted time
+    if (!isActive) {
+      const display = document.getElementById("countdown-display");
+      display.textContent = formatTimeFull(countdownTime);
+    }
+
+    // Save the new countdown time to storage
+    chrome.storage.sync.set({ countdownDurationNext: countdownTime });
+  });
+
+  /**
+   * Checks the current countdown status from the background script.
+   * If the countdown is active, it starts the display update interval and updates the button text.
+   * If the countdown is not active, it stops the display update interval.
+   * This ensures that the popup reflects the current state of the countdown when opened.
+   */
+  chrome.runtime.sendMessage({ action: "getCountdownStatus" }, (status) => {
+    if (status && status.countdownActive) {
+      startDisplayUpdate();
+      isActive = true;
+      updateButton.textContent = "Mettre à jour pour demain";
+    } else {
+      stopDisplayUpdate();
+    }
   });
 });
